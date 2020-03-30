8461e575483fb30bc848d7ed5e2d9a33f86135ec
Kevin Joe <kjjoe11@gmail.com>
2020-03-30 01:48:33 -0400

    removed one of the import lines

--- tftbot.py
+++ tftbot.py
@@ -13,7 +13,6 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 import cv2
 from os import listdir
 from os.path import isfile, join
-from matplotlib import pyplot as plt
 import numpy as np
 import shopdetectlib as sd
 
